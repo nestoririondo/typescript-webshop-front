89c0e5dbe356721502b6326494298326356e02a5
Néstor Iriondo <nestoririondo@gmail.com>
2024-03-26 19:35:13 +0100

    fix: object destructuring

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,6 +1,7 @@
 import { formatCurrency } from "../utils/formatCurrency";
 import { ProductType } from "../types/product";
 import { useBasket } from "../context/useBasket";
+import { useNavigate } from "react-router-dom";
 
 type ItemProps = {
   product: ProductType;
@@ -8,14 +9,25 @@ type ItemProps = {
 
 const Item = ({ product }: ItemProps) => {
   const { addToBasket } = useBasket();
+  const { name, description, price, id } = product;
+
+  const navigate = useNavigate();
 
   return (
-    <div key={product.id.toString()} className="product-card">
-      <h2>{product.name}</h2>
-      <p>{product.description}</p>
-      <p>{formatCurrency(product.price)}</p>
-      <button onClick={() => addToBasket(product.id)}>Add</button>
-    </div>
+    <>
+      <div
+        key={product.id.toString()}
+        onClick={() => navigate(`/products/${product.id}`)}
+        className="product-card"
+      >
+        <h2>{name}</h2>
+        <p>{description}</p>
+        <p>{formatCurrency(price)}</p>
+      </div>
+      <div className="product-card__actions">
+        <button onClick={() => addToBasket(id)}>Add</button>
+      </div>
+    </>
   );
 };
 
