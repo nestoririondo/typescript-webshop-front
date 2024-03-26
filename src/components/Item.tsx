8d5cd47abfaeebaaeb65c6c57f6b09e8eda84766
Néstor Iriondo <nestoririondo@gmail.com>
2024-03-26 19:12:17 +0100

    feat: productList component and styling

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,5 +1,22 @@
-const Item = () => {
-  return <div>Item</div>;
+import { formatCurrency } from "../utils/formatCurrency";
+import { ProductType } from "../types/product";
+import { useBasket } from "../context/useBasket";
+
+type ItemProps = {
+  product: ProductType;
+};
+
+const Item = ({ product }: ItemProps) => {
+  const { addToBasket } = useBasket();
+
+  return (
+    <div key={product.id.toString()} className="product-card">
+      <h2>{product.name}</h2>
+      <p>{product.description}</p>
+      <p>{formatCurrency(product.price)}</p>
+      <button onClick={() => addToBasket(product.id)}>Add</button>
+    </div>
+  );
 };
 
 export default Item;
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,5 +1,19 @@
-const ProductList = () => {
-  return <div>List</div>;
+import { ProductType } from "../types/product";
+import "../styles/productlist.css";
+import Item from "./Item";
+
+type ProductListProps = {
+  products: ProductType[];
+};
+
+const ProductList = ({ products }: ProductListProps) => {
+  return (
+    <div className="product-list">
+      {products.map((product: ProductType) => (
+        <Item key={product.id.toString()} product={product} />
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
