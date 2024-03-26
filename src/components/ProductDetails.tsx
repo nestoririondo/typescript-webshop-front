e18a0260b80ee66440c29e8a944a286ff4f08439
Néstor Iriondo <nestoririondo@gmail.com>
2024-03-26 19:34:21 +0100

    feat: product details page

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,5 +1,19 @@
-const ProductDetails = () => {
-  return <div>Details</div>;
+import { ProductType } from "../types/product";
+import { formatCurrency } from "../utils/formatCurrency";
+
+type ProductTypeProps = {
+  product: ProductType;
+};
+
+const ProductDetails = ({ product }: ProductTypeProps) => {
+  const { name, description, price, id } = product;
+  return (
+    <div className="product">
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <p>{formatCurrency(price)}</p>
+    </div>
+  );
 };
 
 export default ProductDetails;
