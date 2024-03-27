c39b95632666ca69dc3504169a93604c1dd6c9fe
Néstor Iriondo <nestoririondo@gmail.com>
2024-03-27 18:13:31 +0100

    fix: consistent import names on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/useAuth.tsx";
-import { BasketProvider } from "./context/useBasket.tsx";
+import { AuthProvider } from "./context/useAuth";
+import { BasketProvider } from "./context/useBasket";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
