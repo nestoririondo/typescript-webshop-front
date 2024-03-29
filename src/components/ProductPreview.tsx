05be09368a511a415bb146ccecd2d7788f071eb4
Néstor Iriondo <nestoririondo@gmail.com>
2024-03-29 19:01:19 +0100

    style: styling for new additions

--- src/components/ProductPreview.tsx
+++ src/components/ProductPreview.tsx
@@ -9,23 +9,27 @@ type ItemProps = {
 
 const ProductPreview = ({ product }: ItemProps) => {
   const { addToBasket } = useBasket();
-  const { name, description, price, id } = product;
+  const { name, description, price, images, id } = product;
 
   const navigate = useNavigate();
 
   return (
     <>
-      <div
-        key={product.id.toString()}
-        onClick={() => navigate(`/products/${product.id}`)}
-        className="product-card"
-      >
-        <h2>{name}</h2>
-        <p>{description}</p>
-        <p>{formatCurrency(price)}</p>
-      </div>
-      <div className="product-card__actions">
-        <button onClick={() => addToBasket(product)}>Add</button>
+      <div key={product.id.toString()} className="product-card">
+        <div
+          className="product-card-info"
+          onClick={() => navigate(`/products/${id}`)}
+        >
+          <img src={images[0].toString()} alt={name} />
+          <div className="product-card-info-text">
+            <h2>{name}</h2>
+            <p>{description}</p>
+            <p>{formatCurrency(price)}</p>
+          </div>
+        </div>
+        <div className="product-card__actions">
+          <button onClick={() => addToBasket(product)}>Add</button>
+        </div>
       </div>
     </>
   );
