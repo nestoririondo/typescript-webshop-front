c13566e08909cffdce937f9fc319f4c7d5adc1b0
Néstor Iriondo <nestoririondo@gmail.com>
2024-04-08 18:58:13 +0200

    feat: home page supports new Hero and Popular components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,14 @@
+import Hero from "../components/Home/Hero";
+import Popular from "../components/Home/Popular";
+
 const Home = () => {
-  return <>Home</>;
+  return (
+    <>
+      <Hero />
+      <Popular />
+      <h2>Reviews</h2>
+    </>
+  );
 };
 
 export default Home;
