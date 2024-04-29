4b1c23bc2be8a9b036c61a25940a24753d46198f
Néstor Iriondo <nestoririondo@gmail.com>
2024-04-29 14:33:38 +0200

    feat: customer information component

--- src/components/Checkout/CustomerInformation.tsx
+++ src/components/Checkout/CustomerInformation.tsx
@@ -1,33 +1,42 @@
-const CustomerInformation = () => {
+import "../../styles/customer-information.css";
+import { CheckoutStep } from "../../pages/Checkout";
+
+type CustomerInformationProps = {
+  setStep: (step: CheckoutStep) => void;
+  handleChange: (userInput: any) => void;
+};
+const CustomerInformation = ({setStep, handleChange}: CustomerInformationProps) => {
   return (
     <form>
       <div className="form-group">
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" className="form-control" />
+        <input onChange={handleChange} type="text" name="name" className="form-control" />
       </div>
       <div className="form-group">
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" className="form-control" />
+        <input onChange={handleChange } type="email" name="email" className="form-control" />
       </div>
       <div className="form-group">
         <label htmlFor="phone">Phone</label>
-        <input type="tel" id="phone" className="form-control" />
+        <input onChange={handleChange} type="tel" name="phone" className="form-control" />
       </div>
       <div className="form-group">
         <label htmlFor="address">Address</label>
-        <input type="text" id="address" className="form-control" />
+        <input onChange={handleChange} type="text" name="address" className="form-control" />
       </div>
       <div className="form-group">
         <label htmlFor="city">City</label>
-        <input type="text" id="city" className="form-control" />
+        <input onChange={handleChange} type="text" name="city" className="form-control" />
       </div>
       <div className="form-group">
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" className="form-control" />
+        <input onChange={handleChange} type="text" name="postal" className="form-control" />
       </div>
-      <button type="submit" className="btn btn-primary">
-        Continue
-      </button>
+      <section className="next">
+        <button onClick={() => setStep(CheckoutStep.Payment)}>
+          Next
+        </button>
+      </section>
     </form>
   );
 };
