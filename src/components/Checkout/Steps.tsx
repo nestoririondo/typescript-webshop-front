051f7037f6405a2274d6e7b03aa089a54041ff51
Néstor Iriondo <nestoririondo@gmail.com>
2024-04-29 14:35:39 +0200

    feat: developed steps on checkout

--- src/components/Checkout/Steps.tsx
+++ src/components/Checkout/Steps.tsx
@@ -1,4 +1,4 @@
-import { CheckoutStep } from '../../pages/Checkout';
+import { CheckoutStep } from "../../pages/Checkout";
 
 type StepsProps = {
   step: CheckoutStep;
@@ -9,22 +9,51 @@ const Steps = ({ step, setStep }: StepsProps) => {
   return (
     <div className="checkout-steps">
       <div
-        className={`checkout-step ${step === CheckoutStep.OrderSummary ? "active" : ""}`}
+        className={`checkout-step ${
+          step === CheckoutStep.OrderSummary ? "active" : ""
+        }`}
         onClick={() => setStep(CheckoutStep.OrderSummary)}
       >
-        Order Summary
+        <span
+          className={`number ${
+            step === CheckoutStep.OrderSummary ? "active" : ""
+          }`}
+        >
+          1
+        </span>
+        <span className="title">Order Summary</span>
       </div>
+
       <div
-        className={`checkout-step ${step === CheckoutStep.CustomerInformation ? "active" : ""}`}
+        className={`checkout-step ${
+          step === CheckoutStep.CustomerInformation ? "active" : ""
+        }`}
         onClick={() => setStep(CheckoutStep.CustomerInformation)}
       >
-        Customer Information
+        <span
+          className={`number ${
+            step === CheckoutStep.CustomerInformation ? "active" : ""
+          }`}
+        >
+          2
+        </span>
+        <span className="title">Customer Information</span>
       </div>
+      
       <div
-        className={`checkout-step ${step === CheckoutStep.Payment ? "active" : ""}`}
+        className={`checkout-step ${
+          step === CheckoutStep.Payment ? "active" : ""
+        }`}
         onClick={() => setStep(CheckoutStep.Payment)}
       >
-        Payment
+        <span
+          className={`number ${
+            step === CheckoutStep.Payment ? "active" : ""
+          }`}
+        >
+          3
+        </span>
+        <span className="title">Payment</span>
       </div>
     </div>
   );
