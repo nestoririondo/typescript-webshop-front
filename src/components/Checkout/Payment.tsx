ceeab0ced5e21819c8f94fc34aa1ebf0c5ff8a1d
Néstor Iriondo <nestoririondo@gmail.com>
2024-04-29 14:35:17 +0200

    feat: added payment step

--- src/components/Checkout/Payment.tsx
+++ src/components/Checkout/Payment.tsx
@@ -1,5 +1,35 @@
-const Payment = () => {
-  return <h1>Pay</h1>;
+import { CheckoutStep } from "../../pages/Checkout";
+
+type PaymentProps = {
+  setStep: (step: CheckoutStep) => void;
+  handleSubmit: (e: any) => void;
+  handleChange: (e: any) => void;
+};
+
+const Payment = ({ setStep, handleSubmit, handleChange }: PaymentProps) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" className="form-control" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="card">Card Number</label>
+        <input type="text" id="card" className="form-control" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="expiry">Expiry Date</label>
+        <input type="text" id="expiry" className="form-control" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="cvc">CVC</label>
+        <input type="text" id="cvc" className="form-control" />
+      </div>
+      <section className="next">
+        <button>Pay</button>
+      </section>
+    </form>
+  );
 };
 
 export default Payment;
